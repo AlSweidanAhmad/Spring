2a8d2692d357ef8e205d625305b111ed16ed6bcb
AlSweidanAhmad <ahmad.alsweidan01@gmail.com>
2024-03-09 01:06:33 +0100

    IOC #1

---
+++ src/com/spring/FootBall.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class FootBall implements Sport{
+	
+	@Override
+	public void start()
+	{
+		System.out.println("Start FootBall");
+	}
+
+}
---
+++ src/com/spring/VolleyBall.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+public class VolleyBall implements Sport{
+	
+	@Override
+	public void start()
+	{
+		System.out.println("Start BollyBall");
+	}
+
+}
